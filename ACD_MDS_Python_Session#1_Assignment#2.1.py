1fff0c2e2e7d4d7e83d1e54a4e355fa3913fe2df
Sreenivasan V Rajangan <rvsreeni@gmail.com>
2017-11-12 17:05:25 +0530

    Add files via upload

---
+++ ACD_MDS_Python_Session#1_Assignment#2.1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Accepts a sequence of comma separated numbers from console and generate a list
+"""
+
+inp_seq = str(input("Type in a sequence of comma seperated numbers: "))
+print(inp_seq.split(","))
