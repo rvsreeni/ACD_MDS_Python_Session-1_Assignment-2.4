1182a37fb62a75b99f605717402c32c12d3e4dad
Sreenivasan V Rajangan <rvsreeni@gmail.com>
2017-11-12 09:21:27 +0530

    Add files via upload

---
+++ ACD_MDS_Python_Session#1_Assignment#2.4.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Print the given string in the format specified in the sample output.
+
+"""
+inp_str = """
+WE, THE PEOPLE OF INDIA,{}having solemnly resolved to constitute India into a SOVEREIGN, {}SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC{}and to secure to all its citizens{}
+"""
+print(inp_str.format("\n\t","!\n\t\t","\n\t\t ",":"))
