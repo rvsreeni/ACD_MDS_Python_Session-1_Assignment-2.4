2a1bea99070abb0338c1b3e2b24b02e8b9e67885
Sreenivasan V Rajangan <rvsreeni@gmail.com>
2017-12-23 19:51:30 +0530

    Delete ACD_MDS_Python_Session#1_Assignment#2.1.py

--- ACD_MDS_Python_Session#1_Assignment#2.1.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Accepts a sequence of comma separated numbers from console and generate a list
-"""
-
-inp_seq = str(input("Type in a sequence of comma seperated numbers: "))
-print(inp_seq.split(","))
